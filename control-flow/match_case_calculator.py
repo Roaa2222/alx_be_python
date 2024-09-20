a455296bae1c77ed8e73c503a14880ad02d00c30
Roaa2222 <roaa1602@gmail.com>
2024-09-20 13:46:35 -0700

    done

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+#num1 = int(input("Enter the first number = "))
+#num2 = int(input("Enter the second number = "))
+#operation_type = (input("Choose the operation (+,-,*,/) :  "))
+#match operation_type :
+      #case ("+") :
+           #result = num1 + num2
+           #print("The result is" , result)
+      #case ("-") :
+           #result = num1 - num2
+           #print("The result is ", result)
+      #case ("*"):
+           #result = num1 * num2
+           #print("the result is" , result)
+      #case ("/"):
+           #if num2 == 0:
+              #print("Cannot divide by zero.")
+           #else:
+              #result = num1 / num2 
+              #print("the result is" , result)
+      #case _:
+           #print("Invalid operation. Please choose one of +, -, *, /.")    
+    
