493443ef636be375daab04d592f27c39548333a5
Roaa2222 <roaa1602@gmail.com>
2024-10-03 16:24:07 -0700

    done

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,15 +1,11 @@
-def safe_divide(numerator , denomenator):
-     division = numerator/denomenator
-     return(division)
-try:
-    numerator = int (input("Enter the numerator : "))
-    denomenator = int (input("Enter the denomenator : "))
-    if denomenator != 0:
-      result =safe_divide(numerator , denomenator) 
-      print (f"the result is {result}")
-    else:
-      raise ZeroDivisionError("Invalid denomenator. Please enter the the right denomenator")
-except ValueError:
-  print("please enter an integer number")
-except ZeroDivisionError as e:
-    print(e) 
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        denom = float(denominator)
+        
+        result = num / denom
+        return f"The result of the division is {result}"
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
